60196542bc511481ec0798c6d937b2544e93db5e
Ryan Toh <ryantohhongrui@hotmail.com>
2025-10-07 09:28:54 +0800

    stack: improve test formatting and fix memory leak after pop

--- tests/test_stack.c
+++ tests/test_stack.c
@@ -1,5 +1,6 @@
 #include "stack.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void print_stack(Stack *stack) {
         if (!stack) {
@@ -17,11 +18,14 @@ void print_stack(Stack *stack) {
 
 int main(void) {
 
-        printf("--- CREATING STACK ---\n");
+        int i;
+        int *j;
+
+        printf("\n--- CREATING STACK ---\n");
         Stack *stack = create_stack();
 
-        printf("--- PUSHING ITEMS ---\n");
-        for (int i = 0; i < 5; i++) {
+        printf("\n--- PUSHING ITEMS ---\n");
+        for (i = 0; i < 5; i++) {
                 push(stack, &i, sizeof(int));
         }
 
@@ -30,16 +34,19 @@ int main(void) {
         printf("Length: %zu nodes\n", stack->length);
         printf("Peek first item: %d\n", *(int *)peek(stack));
 
-        printf("--- POPPING ITEMS ---");
-        pop(stack);
-        pop(stack);
+        printf("\n--- POPPING ITEMS ---\n");
+        for (i = 0; i < 2; i++) {
+                j = (int *)pop(stack);
+                printf("Popped: %d\n", *j);
+                free(j);
+        }
 
         printf("Stack after popping: ");
         print_stack(stack);
         printf("Length: %zu nodes\n", stack->length);
         printf("Peek first item: %d\n", *(int *)peek(stack));
 
-        printf("--- FREEING QUEUE ---\n");
+        printf("\n--- FREEING QUEUE ---\n");
         free_stack(stack);
 
         printf("END\n");
