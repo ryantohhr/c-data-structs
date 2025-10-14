417c82632743bcd75cae78a9c9ec1e37bb13e0a1
Ryan Toh <ryantohhongrui@hotmail.com>
2025-10-14 12:38:55 +0800

    queue: add test comments, remove redundant assert

--- tests/test_queue.c
+++ tests/test_queue.c
@@ -22,22 +22,26 @@ int main(void) {
         int i;
         int *j;
 
+        // Test create_queue
         Queue *queue = create_queue();
         assert(queue != NULL);
 
+        // Test enqueue
         for (int i = 0; i < 5; i++) {
                 enqueue(queue, &i, sizeof(i));
         }
         assert(queue->length == 5);
+
+        // Test peek
         assert(*(int *)peek(queue) == 0);
 
+        // Test dequeue
         for (i = 0; i < 2; i++) {
                 j = (int *)dequeue(queue);
                 assert(*j == i);
                 free(j);
                 j = NULL;
         }
-        assert(j == NULL);
         assert(queue->length == 3);
 
         free_queue(queue);
